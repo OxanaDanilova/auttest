3691b8e5e4d18074e04cc8884a24ffd76d21d18f
OxanaDanilova <oxanadanilova11@gmail.com>
2019-11-17 17:29:17 +0100

    feat: add authentification

--- src/index.js
+++ src/index.js
@@ -472,6 +472,23 @@ const main = () => {
     ctx.putImageData(imageData, 0, 0);
   };
   wbBtn.addEventListener('click', grayscale);
+
+
+  //    ////////////////////////
+
+  const anchorTag = document.getElementById('login')
+  const outputText = document.getElementById('output')
+  anchorTag.addEventListener('click', (e) => {
+    e.preventDefault()
+    const authenticator = new netlify.default ({})
+    authenticator.authenticate({provider:"github", scope: "user"}, (err, data) => {
+      err ? outputText.innerText = "Error Authenticating with GitHub: " + err :
+      outputText.innerText = "Authenticated with GitHub. Access Token: " + data.token
+    })
+  })
+
+  //  /////////////////////////////////
+
 };
 
 main();
